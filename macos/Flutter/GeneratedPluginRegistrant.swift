e0a1b0757492a394e201289dcd9e670b0cd6f6f0
James-Wiley <james.wiley@live.com>
2026-04-17 23:45:52 -0500

    add libraries for animations (maybe)

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
+import file_picker
 import flutter_blue_plus_darwin
 import hand_detection
+import just_audio
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   HandDetectionPlugin.register(with: registry.registrar(forPlugin: "HandDetectionPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
 }
